8b1a9e7d5b9d356eb20662f19740e8a23169ef15
JONAAVILA <jona_1969@hotmail.com>
2024-10-02 11:54:40 -0300

    cookie aplieted and checkemail funtion fixed

--- src/adapters/checkEmail.js
+++ src/adapters/checkEmail.js
@@ -1,10 +1,13 @@
 import axios from "axios";
 
-const URL = 'https://tienda-online-backend-riqf.onrender.com/users/check/email'
+// const URL = 'https://tienda-online-backend-riqf.onrender.com/users/check/email'
+const URL = 'http://localhost:3000/users/check/email'
 
 export default async function checkEmail(email){
     console.log('adapter',email)
-    const res = await axios.post(URL,{email:email})
+    const res = await axios.post(URL,{email:email},{
+        withCredentials: true
+    })
     console.log('data',res.data)
     return res.data
 }
