5cc3ee2be0d994a3c736358a1177a55606d49778
JONAAVILA <jona_1969@hotmail.com>
2024-10-05 12:10:43 -0300

    landign view created

---
+++ src/views/landing/Landing.jsx
@@ -0,0 +1,12 @@
+import './landing.css';
+
+const Landing = ()=>{
+    return(
+        <section className='landing_section' >
+            <h1>todo para tu bebe</h1>
+            <h2>Hacemos envios a dominicilio atravez de mercado pago</h2>
+        </section>
+    )
+}
+
+export default Landing
