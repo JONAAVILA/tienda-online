009909f0afcf2259231008e38b04717b823ceac9
JONAAVILA <jona_1969@hotmail.com>
2024-10-06 22:40:52 -0300

    working at the navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -4,7 +4,7 @@ import './navbar.css'
 const Navbar = ()=>{
     return(
         <section className="navbar_section" >
-            <div className="nav_profile" />
+            <a className="nav_profile" href="/"/>
             <div className="nav_box_pages" >
                 <a href="">nosotros</a>
                 <a href="">contacto</a>
