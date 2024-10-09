a0a51fa74524a1a3f44e07010cac6b01706b7705
JONAAVILA <jona_1969@hotmail.com>
2024-10-09 12:10:21 -0300

    create modal validation code

--- src/components/modals/validateCode/ValidateCode.modals.jsx
+++ src/components/modals/validateCode/ValidateCode.modals.jsx
@@ -6,17 +6,20 @@ const ValidateCode = ()=>{
     const email = useSelector(state => state.user.email)
 
     return(
-        <div className="code_box" >
-            <h1>valida tu código</h1>
-            <p>te lo enviamos a 📮 {email}, revisa tu casilla de spam 😎</p>
-            <form action="">
-                <input
-                    type="text"
-                    placeholder="code"
-                />
-                <ButtonCircle color={'natural'} />
-            </form>
-        </div>
+        <>
+            <div className="code_container"/ >
+            <div className="code_box" >
+                <h1>valida tu código</h1>
+                <p>te lo enviamos a 📮 {email}, revisa tu casilla de spam 😎</p>
+                <form action="">
+                    <input
+                        type="text"
+                        placeholder="code"
+                    />
+                    <ButtonCircle color={'natural'} />
+                </form>
+            </div>
+        </>
     )
 }
 
