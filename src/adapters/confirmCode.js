860d8934dfa51cc009fe40042537ce04ae09bb55
JONAAVILA <jona_1969@hotmail.com>
2024-10-10 12:46:47 -0300

    create user complete

--- src/adapters/confirmCode.js
+++ src/adapters/confirmCode.js
@@ -3,7 +3,8 @@ import axios from "axios"
 const URL = 'http://localhost:3000/singin/check/code'
 
 export async function confirmCode(code){
-    const res = await axios.post(URL,code,{
+    const res = await axios.post(URL,{code:code},{
         withCredentials:true
     })
-}
+    return res.data
+}   
